59238ec2eb22d192bdcd505ea6452e9b6d810bdf
Emma James <ejames@companieshouse.gov.uk>
2020-04-28 20:59:30 +0100

    Added javascript for selected documents
    Both bottom and top counter working.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,14 +41,15 @@ $("input[name='documentType']").on('change', function () {
 $("input[name='rowCheckBox']").on('change', function () {
   if ($(this).prop('checked') === false) {
       selectedDocuments--;
+      var newText = "";
       if(selectedDocuments == 0){
-        $("#documentsSelectedCount").text("No documents selected.");
+        newText = "No documents selected"
       }
       else if(selectedDocuments == 1){
-        $("#documentsSelectedCount").text(selectedDocuments + " Document selected:");
+        newText = "1 document selected"
       }
       else if(selectedDocuments > 1){
-        $("#documentsSelectedCount").text(selectedDocuments + " Documents selected:");
+        newText = selectedDocuments + " documents selected"
       }
       var element = $(this).closest('tr').index();
       $("#ul"+ element).remove();
@@ -56,15 +57,15 @@ $("input[name='rowCheckBox']").on('change', function () {
     } else {
       selectedDocuments++;
       if(selectedDocuments == 1){
-        $("#documentsSelectedCount").text(selectedDocuments + " Document selected:");
+        newText = "1 document selected"
       }
       else if(selectedDocuments > 1){
-        $("#documentsSelectedCount").text(selectedDocuments + " Documents selected:");
+        newText = selectedDocuments + " documents selected"
       }
-
       var element = $(this).closest('tr').index();
       var description = $(this).closest('td').nextAll().last().text();
       $("#documentsSelectedList").append("<li id='ul" + element + "'>" + description + "</li>");
   }
-
+  $("#documentsSelectedCountTop").text(newText);
+  $("#documentsSelectedCountBottom").text(newText);
 })
